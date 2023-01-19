3cb559be56184354c52e477f470adf709ba5b4a1
Lortiz528 <lortiztech@gmail.com>
2023-01-18 23:38:29 -0500

    google map link

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -7,7 +7,7 @@ function Contact() {
       <h2 className="contact-header">Contact Us</h2>
       <section className="details">
         <h4>Monday - Sunday 11AM - 10PM </h4>
-        <h4>1716 Utica Ave Brooklyn, NY 11234</h4>
+        <h4><a href='https://www.google.com/search?q=Cecilia%27s+Pizzeria&tbm=lcl&ei=2sjIY-3ZH4Cr5NoPr4CIwAc&ved=0ahUKEwjtrqHW59L8AhWAFVkFHS8AAngQ4dUDCAk&uact=5&oq=Cecilia%27s+Pizzeria&gs_lcp=Cg1nd3Mtd2l6LWxvY2FsEAMyBwgAEIAEEAoyBwgAEIAEEAoyBwgAEIAEEAoyBwgAEIAEEAoyBwgAEIAEEAoyBwgAEIAEEAoyBQgAEIAEMgYIABAWEB4yBggAEBYQHjIGCAAQFhAeOgUIIRCrAjoHCCEQoAEQCjoICCEQFhAeEB06BQgAEIYDOgkIABCABBANEAo6CAgAEBYQHhAKOgoIABCABBCxAxAKOgUIABCRAjoNCAAQgAQQsQMQgwEQCjoICAAQsQMQgwE6CwgAEIAEELEDEIMBOggIABCABBCxAzoGCAAQChBDOgQIABBDOgsIABCxAxCDARCRAjoICAAQsQMQkQJQ_QNY_ktgmVJoAXAAeACAAXOIAZETkgEENDEuMZgBAKABAbABAMABAQ&sclient=gws-wiz-local#rlfi=hd:;si:10696701272716308438,l,ChJDZWNpbGlhJ3MgUGl6emVyaWFI9NPjrJaxgIAIWhwQABABGAAYASISY2VjaWxpYSdzIHBpenplcmlhkgEQcGl6emFfcmVzdGF1cmFudOABAA,y,T_HG9FxkiGQ;mv:[[40.630440877319025,-73.92828485307811],[40.63008092268097,-73.92875914692189]]'  style={{ textDecoration: 'none' }}>1716 Utica Ave Brooklyn, NY 11234</a></h4>
         <h4>ceciliaspizza@gmail.com</h4>
         <h4>(718)-484-7495 or (718)-484-7496</h4>
       </section>
