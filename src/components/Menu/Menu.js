b46e76f7434558586d19a01c1a86dbb2e1fae55d
Lortiz528 <lortiztech@gmail.com>
2023-01-31 14:18:58 -0500

    updated menu images

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -7,9 +7,9 @@ function Menu() {
     <Container  id='menuSection' className='menuContainer'>
       <h4>Menu</h4>
       <section className='menuSection'>
-        <img className='menuImage' src='https://i.imgur.com/btiPHSa.png' alt='menu1'/>
+        <img className='menuImage' src='https://i.imgur.com/Scs98iE.png' alt='menu1'/>
       <br></br>
-        <img className='menuImage' src='https://i.imgur.com/PhvZnZQ.png' alt='menu2'/>
+        <img className='menuImage' src='https://i.imgur.com/K9OyI8d.png' alt='menu2'/>
       </section>
       <footer>* Menu Items and prices are subject to change. Please visit or contact us for latest updates *</footer>
     </Container>
